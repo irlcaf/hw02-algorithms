c4cbbdc35f19dccde7cf8bbf5500ba78bd48823a
irlcaf <carlomagnoaf@hotmail.com>
2019-03-31 19:40:30 +0800

    final

---
+++ hw02.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def findPeak(array,low,high,length):
+    mid = low + (high-low)/2
+    mid = int(mid)
+
+    if((mid==0 or array[mid-1] <= array[mid]) and (mid == length-1 or array[mid+1] <= array[mid])):
+        return mid
+    elif (mid > 0 and array[mid-1] > array[mid]):
+        return findPeak(array,low,(mid-1),length)
+    else:
+        return findPeak(array,(mid+1),high,length)
+
+filename_1 = open(raw_input("Please enter the file name with the array of integers:"),'r')
+lines = filename_1.readlines()
+for line in lines:
+    array = np.fromstring(line,dtype=int,sep=",")
+filename_1.close()
+print(array)
+length = len(array)
+
+print("The peak point is: ",array[findPeak(array,0,length-1,length)])
