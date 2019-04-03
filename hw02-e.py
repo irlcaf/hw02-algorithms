98ed852686b702e6e0d35900da15f3009c3248eb
irlcaf <carlomagnoaf@hotmail.com>
2019-04-03 15:51:08 +0800

    asd

--- hw02-e.py
+++ hw02-e.py
@@ -3,10 +3,12 @@ def findPeak(array):
     low=0
     high= len(array)-1
     length = len(array)-1
+    if np.count_nonzero(array == array[0]) == len(array):
+        print("All the elements are the same, peak is:%d" %array[0])
     if array[low] > array[low+1]:
-        return low
+        print("Lower corner is a peak: %d"%array[low])
     elif array[high] > array[high-1]:
-        return high
+        print("Upper corner is a peak: %d"%array[high])
     else:
         while True:
             mid = (high+low)//2
@@ -15,11 +17,12 @@ def findPeak(array):
             elif mid < length and array[mid] < array[mid+1]:
                 low = mid
             else: 
-                return mid
+                print("The peak is:%d "%array[mid])
+                break
             
 filename_1 = open(raw_input("Insert the text file:"),'r')
 lines = filename_1.readlines()
 for line in lines:
    array = np.fromstring(line,dtype=int,sep=',')
 filename_1.close()
-print("The peak point is: %d"%array[findPeak(array)])
+findPeak(array)
